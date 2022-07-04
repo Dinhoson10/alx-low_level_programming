3b25bdfe6e244e1dacfa538507c3c4972f9db61d
Dinhoson10 <azebioyeinkurokakemo@gmail.com>
2022-07-04 05:28:55 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ * return: Always 0 (succes)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece art,\n");
+return (0);
+}
+
