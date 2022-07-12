63a6941dc6744a28231f86df7729192288c277f5
Dinhoson10 <azebioyeinkurokakemo@gmail.com>
2022-07-12 14:06:56 -0700

    m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,4 +12,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
