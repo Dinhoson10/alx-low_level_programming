b2d215b3766f0e30e866962a757649ef70e71d2c
Dinhoson10 <azebioyeinkurokakemo@gmail.com>
2022-07-12 15:12:16 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * @s: argument to the function
- * Description: return the required results
+ * @s: argument to tbe function
+ * Description: return the required result
  * Return: return integer value
  */
 int _strlen(char *s)
@@ -13,5 +13,5 @@ while (*s != '\0')
 count += 1;
 s++;
 }
-return (count)
+return (count);
 }
